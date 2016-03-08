ac8b1548da3db11a7c83db2913d12116fb43181c
nestier <fernan.nestier@gmail.com>
2016-03-08 19:19:39 -0300

    add link.sh script

---
+++ link.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# files=(vimrc vim zsh sarasa)
+for file in $(ls); do
+    destination="${HOME}/.${file}"
+    # [ -e "${destination}" ] && rm -fr $destination
+    [ -e $destination -o -h $destination ] && rm -rf $destination
+    ln -s $file $destination
+done
